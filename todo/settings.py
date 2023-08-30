b4478787b401fcd21d1a3bc26bc00bec237e7b05
10401996Manas    /    10401996Manas <Manasiya9600@gmail.com>
2023-08-30 19:03:13 +0600

    threename

--- todo/settings.py
+++ todo/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates", BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
